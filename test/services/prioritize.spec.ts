3d546813342af056363765097e47363ac5b8a42f
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-08-29 08:20:18 +0430

    test:complete tests

---
+++ test/services/prioritize.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+
+import prioritize from '../../src/services/prioritize';
+import DataRepo from '../../src/repository/dataRepo';
+import { Transaction } from '../../src/types';
+
+const LatenciesTable = DataRepo.getLatencies();
+
+function _calculateTotalTime(transactions: Transaction[]): number {
+  return transactions
+    .map((tr) => LatenciesTable[tr.bankCountryCode])
+    .reduce((acc, number) => acc + number, 0);
+}
+suite('Testing prioritize services:', () => {
+  test('should  prioritize services works correctly', async () => {
+    const sampleTransactions = await DataRepo.getTransactions();
+
+    [90, 60, 50].forEach((timeLimit) => {
+      const { totalMoney, usedTransaction } = prioritize(
+        sampleTransactions,
+        timeLimit,
+        LatenciesTable
+      );
+      expect(usedTransaction).to.be.an('array');
+      expect(_calculateTotalTime(usedTransaction)).lessThanOrEqual(timeLimit);
+    });
+  });
+});
